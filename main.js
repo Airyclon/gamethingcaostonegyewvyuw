41f2dcd9898aefb3c9324739d6e9f9ff29f2b3f1
Airyclon <84323073+Airyclon@users.noreply.github.com>
2022-04-13 19:00:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function lose(){
+    window.location = "lose.html";
+}
+function win(){
+    window.location = "win.html";
+}
+function last(){
+    window.location = "last.html";
+}
+function gg(){
+    document.getElementById("gglab").inerHTML = "U WIN GGGGGGG!!!!! :)";
+}
